69bf7ac1fa920fa035b2db15abf4a8d46ab93d7d
CITdsatoh <68046473+CITdsatoh@users.noreply.github.com>
2022-06-29 13:07:22 +0900

    Create mk_pofin_corners.js

---
+++ pofin/mk_pofin_corners.js
@@ -0,0 +1,217 @@
+
+
+const all_nuts=[]; 
+all_nuts.push(["クラボ",[1,0,0,0,0],25]);
+all_nuts.push(["カゴ　",[0,1,0,0,0],25]);
+all_nuts.push(["モモン",[0,0,1,0,0],25]);
+all_nuts.push(["チーゴ",[0,0,0,1,0],25]);
+all_nuts.push(["ナナシ",[0,0,0,0,1],25]);
+all_nuts.push(["ヒメリ",[1,0,1,1,1],20]);
+all_nuts.push(["オレン",[1,1,0,1,1],20]);
+all_nuts.push(["キー　",[1,1,1,0,1],20]);
+all_nuts.push(["ラム　",[1,1,1,1,0],20]);
+all_nuts.push(["オボン",[0,1,1,1,1],20]);
+all_nuts.push(["フィラ",[2,0,0,0,0],25]);
+all_nuts.push(["ウイ　",[0,2,0,0,0],25]);
+all_nuts.push(["マゴ　",[0,0,2,0,0],25]);
+all_nuts.push(["バンジ",[0,0,0,2,0],25]);
+all_nuts.push(["イア　",[0,0,0,0,2],25]);
+all_nuts.push(["ズリ　",[2,1,0,0,0],20]);
+all_nuts.push(["ブリー",[0,2,1,0,0],20]);
+all_nuts.push(["ナナ　",[0,0,2,1,0],20]);
+all_nuts.push(["セシナ",[0,0,0,2,1],20]);
+all_nuts.push(["パイル",[1,0,0,0,2],20]);
+all_nuts.push(["ザロク",[1,0,1,1,0],20]);
+all_nuts.push(["ネコブ",[0,1,0,1,1],20]);
+all_nuts.push(["タポル",[1,0,1,0,1],20]);
+all_nuts.push(["ロメ　",[1,1,0,1,0],20]);
+all_nuts.push(["ウブ　",[0,1,1,0,1],20]);
+all_nuts.push(["マトマ",[3,1,0,0,0],30]);
+all_nuts.push(["モコシ",[0,3,1,0,0],30]);
+all_nuts.push(["ゴス　",[0,0,3,1,0],30]);
+all_nuts.push(["ラブタ",[0,0,0,3,1],30]);
+all_nuts.push(["ノメル",[1,0,0,0,3],30]);
+all_nuts.push(["ノワキ",[5,1,0,0,0],35]);
+all_nuts.push(["シーヤ",[0,5,1,0,0],35]);
+all_nuts.push(["カイス",[0,0,5,1,0],35]);
+all_nuts.push(["ドリ　",[0,0,0,5,1],35]);
+all_nuts.push(["ベリブ",[1,0,0,0,5],35]);
+all_nuts.push(["オッカ",[2,0,1,0,0],30]);
+all_nuts.push(["イトケ",[0,2,0,1,0],30]);
+all_nuts.push(["ソクノ",[0,0,2,0,1],30]);
+all_nuts.push(["リンド",[1,0,0,2,0],30]);
+all_nuts.push(["ヤチェ",[0,1,0,0,2],30]);
+all_nuts.push(["ヨプ　",[2,0,0,1,0],30]);
+all_nuts.push(["ビアー",[0,2,0,0,1],30]);
+all_nuts.push(["シュカ",[1,0,2,0,0],30]);
+all_nuts.push(["バコウ",[0,1,0,2,0],30]);
+all_nuts.push(["ウタン",[0,0,1,0,2],30]);
+all_nuts.push(["タンガ",[3,0,0,0,1],35]);
+all_nuts.push(["ヨロギ",[1,3,0,0,0],35]);
+all_nuts.push(["カシブ",[0,1,3,0,0],35]);
+all_nuts.push(["ハバン",[0,0,1,3,0],35]);
+all_nuts.push(["ナモ　",[0,0,0,1,3],35]);
+all_nuts.push(["リリバ",[4,1,0,0,0],35]);
+all_nuts.push(["ホズ　",[0,4,1,0,0],35]);
+all_nuts.push(["チイラ",[5,1,5,0,0],40]);
+all_nuts.push(["リュガ",[0,5,1,5,0],40]);
+all_nuts.push(["カムラ",[0,0,5,1,5],40]);
+all_nuts.push(["ヤタピ",[5,0,0,5,1],40]);
+all_nuts.push(["ズア　",[1,5,0,0,5],40]);
+all_nuts.push(["サン　",[5,1,5,1,5],50]);
+all_nuts.push(["スター",[5,1,5,1,5],50]);
+all_nuts.push(["ナゾ　",[7,1,0,0,0],60]);
+all_nuts.push(["ミクル",[0,7,1,0,0],60]);
+all_nuts.push(["イバン",[0,0,7,1,0],60]);
+all_nuts.push(["ジャポ",[0,0,0,7,1],60]);
+all_nuts.push(["レンブ",[1,0,0,0,7],60]);
+all_nuts.push(["ロゼル",[0,0,4,1,0],35]);
+all_nuts.push(["アッキ",[5,5,1,1,1],55]);
+all_nuts.push(["タラプ",[1,1,5,5,1],55]);
+all_nuts.push(["グレプ",[5,1,1,1,5],55]);
+all_nuts.push(["カット",[1,5,5,1,1],55]);
+all_nuts.push(["ルーケ",[1,1,1,5,5],55]);
+all_nuts.push(["マーピ",[9,1,0,0,0],80]);
+all_nuts.push(["ナンギ",[0,9,1,0,0],80]);
+all_nuts.push(["ソマメ",[0,0,9,1,0],80]);
+all_nuts.push(["ダコン",[0,0,0,9,1],80]);
+all_nuts.push(["ニンジ",[1,0,0,0,9],80]);
+all_nuts.push(["カドボ",[5,0,3,0,0],50]);
+all_nuts.push(["シスカ",[0,5,0,3,0],50]);
+all_nuts.push(["ミパ　",[0,0,5,0,3],50]);
+all_nuts.push(["ウリキ",[3,0,0,5,0],50]);
+all_nuts.push(["イーサ",[0,3,0,0,5],50]);
+all_nuts.push(["カムカ",[5,0,0,3,0],50]);
+all_nuts.push(["カップ",[0,5,0,0,3],50]);
+all_nuts.push(["ピーナ",[3,0,5,0,0],50]);
+all_nuts.push(["ミグ　",[0,3,0,5,0],50]);
+all_nuts.push(["コウ　",[0,0,3,0,5],50]);
+all_nuts.push(["クチナ",[6,0,0,0,0],55]);
+all_nuts.push(["アエロ",[0,6,0,0,0],55]);
+all_nuts.push(["ネクタ",[0,0,6,0,0],55]);
+all_nuts.push(["ピスタ",[0,0,0,6,0],55]);
+all_nuts.push(["シーク",[0,0,0,0,6],55]);
+all_nuts.push(["アセロ",[2,1,2,0,0],15]);
+all_nuts.push(["リーク",[0,2,1,2,0],15]);
+all_nuts.push(["マンダ",[0,0,2,1,2],15]);
+all_nuts.push(["コマナ",[2,0,0,2,1],15]);
+all_nuts.push(["エミル",[1,2,0,0,2],15]);
+all_nuts.push(["ズラカ",[6,0,4,2,0],60]);
+all_nuts.push(["ベム　",[0,6,0,4,2],60]);
+all_nuts.push(["トーチ",[2,0,6,0,4],60]);
+all_nuts.push(["タロイ",[4,2,0,6,0],60]);
+all_nuts.push(["メマロ",[0,4,2,0,6],60]);
+all_nuts.push(["リスグ",[6,0,2,0,4],60]);
+all_nuts.push(["ケノキ",[4,6,0,2,0],60]);
+all_nuts.push(["ヤマモ",[0,4,6,0,2],60]);
+all_nuts.push(["シイ　",[2,0,4,6,0],60]);
+all_nuts.push(["コーヤ",[0,2,0,4,6],60]);
+all_nuts.push(["ループ",[6,2,0,4,0],60]);
+all_nuts.push(["メツ　",[0,6,2,0,4],60]);
+all_nuts.push(["モーア",[4,0,6,2,0],60]);
+all_nuts.push(["ゲイン",[0,4,0,6,2],60]);
+all_nuts.push(["メウ　",[2,0,4,0,6],60]);
+all_nuts.push(["ガショ",[8,1,0,0,0],70]);
+all_nuts.push(["ズキア",[0,8,1,0,0],70]);
+all_nuts.push(["ルミク",[0,0,8,1,0],70]);
+all_nuts.push(["コノキ",[0,0,0,8,1],70]);
+all_nuts.push(["ボスカ",[1,0,0,0,8],70]);
+all_nuts.push(["パプリ",[7,0,5,3,1],85]);
+all_nuts.push(["コケモ",[1,7,0,5,3],85]);
+all_nuts.push(["ココナ",[3,1,7,0,5],85]);
+all_nuts.push(["ロズヒ",[5,3,1,7,0],85]);
+all_nuts.push(["ズユ　",[0,5,3,1,7],85]);
+all_nuts.push(["ナトゥ",[5,1,5,5,1],55]);
+all_nuts.push(["アロニ",[1,5,1,5,5],55]);
+all_nuts.push(["ポーポ",[5,1,5,1,5],55]);
+all_nuts.push(["クワマ",[5,5,1,5,1],55]);
+all_nuts.push(["フジョ",[1,5,5,1,5],55]);
+
+
+
+  
+  function calc(){
+  
+     let selections=document.querySelectorAll("select.nuts");
+     let pofin_maker=new PofinMaker()
+     for(var one_person of selections){
+       var selected_index=one_person.options.selectedIndex;
+       var selected_value=one_person.options[selected_index].value;
+       var nuts_no=parseInt(selected_value);
+       if(nuts_no !==0){
+         nuts_info=all_nuts[nuts_no-1];
+         pofin_maker.addNuts(new Nuts(nuts_no,nuts_info[0],nuts_info[1],nuts_info[2]));
+       }
+     }
+     
+     let made_pofin=pofin_maker.getPofin();
+     dispInfo(made_pofin);
+       
+  
+  }
+  
+  function mkOption(){
+    
+    let all_selections=document.querySelectorAll("select.nuts");
+    for(var one_selection of all_selections){
+      for(var i=0;i<all_nuts.length;i++){
+        var one_option=document.createElement("option");
+        one_option.value=""+(i+1);
+        one_option.text="No."+(i+1)+":"+all_nuts[i][0];
+        one_selection.appendChild(one_option);
+      }
+   }
+   
+   let accordition_time_selection=document.querySelector("select[name='accordition']");
+   
+   
+   for(var i=0;i<=10;i++){
+     var one_option=document.createElement("option");
+     one_option.value=""+i;
+     one_option.text=""+i+"回";
+     accordition_time_selection.appendChild(one_option);
+   }
+  
+  }
+   
+  function dispInfo(pofin_info){
+    
+    let pofin_name_input=document.querySelector("input[name='pofin_name']");
+    pofin_name_input.value=pofin_info.getPofinName();
+     
+    let pofin_level_input=document.querySelector("input[name='pofin_level']");
+    pofin_level_input.value=pofin_info.getLevel();
+     
+    let pofin_taste_inputs=document.querySelectorAll("input.tastes");
+    for(var i=0;i<pofin_taste_inputs.length;i++){
+       pofin_taste_inputs[i].value=pofin_info.tastes[i];
+    }
+     
+     
+    let pofin_smooth_input=document.querySelector("input[name='pofin_smooth']");
+    pofin_smooth_input.value=pofin_info.smooth;
+     
+    let pofin_rate_input=document.querySelector("input[name='pofin_rate']");
+    pofin_rate_input.value=pofin_info.getRate();
+     
+     
+    let pofin_made_history=document.querySelector("textarea[name='pofin_history']");
+    pofin_made_history.value=pofin_made_history.value+pofin_info+"\n";
+     
+  }
+  
+  function resetAll(){
+    let all_selects=document.querySelectorAll("select");
+    for(var one_select of all_selects){
+      one_select.selectedIndex=0;
+    }
+    
+    let all_text_inputs=document.querySelectorAll("input[type='text'],textarea");
+    for(var one_text_input of all_text_inputs){
+      one_text_input.value="";
+    }
+    
+  }
+  
+ 
+   
